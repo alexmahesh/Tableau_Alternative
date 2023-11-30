b94160ed1b3d939c3f19613f87d403347b24d434
alexmahesh <mail@alexanderschuppe.de>
2023-11-30 11:15:40 +0100

    Implemented data visualization with PyGWalker in app.py file.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as stc
 import pandas as pd
-import pygwalker as pyg
+from pygwalker.api.streamlit import init_streamlit_comm, get_streamlit_html
 
 
 @st.cache_resource
@@ -14,24 +14,37 @@ def load_data(data):
     return pd.read_csv(data)
 
 
+@st.cache_resource
+def get_pyg_html(df):
+    '''
+    Returns PyGWalker with the Data as html suitable for Streamlit.
+    '''
+    html = get_streamlit_html(df, spec="./gw0.json", use_kernel_calc=True, debug=False)
+    return html
+
+
 def main():
     df = None
-    
     st.set_page_config(
         page_title = "Small Tableau",
         layout = "wide"
     )
     st.title("Small Tableau")
     
+    init_streamlit_comm() # Initialize pygwalker communication
+    
     with st.sidebar:
         st.header("Upload")
         with st.form("Upload CSV-File"):
             file = st.file_uploader("Upload a .csv file", type=["csv"])
             submitted = st.form_submit_button("Submit")
+            
     if submitted:
         df = load_data(file)
-        st.dataframe(df)
-
+        with st.expander("Data Table"):
+            st.dataframe(df)
+        # Visualize Data with PyGWalker
+        stc.html(get_pyg_html(df), width=1300, height=1000, scrolling=True)
 
 
 if __name__ == '__main__':
