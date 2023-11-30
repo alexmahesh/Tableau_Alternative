ece3f7d48c0700862a2953672fb2b9ee5e132739
alexmahesh <mail@alexanderschuppe.de>
2023-11-30 10:32:53 +0100

    Added load_data function to app.py file.

--- app.py
+++ app.py
@@ -3,3 +3,21 @@ import streamlit.components.v1 as stc
 import pandas as pd
 import pygwalker as pyg
 
+
+@st.cache_resource
+def load_data(data):
+    '''
+    Loads a CSV-File and returns it as a pandas dataframe.
+    @data (csv): The file in CSV-Format.
+    @return (pd.Dataframe): The loaded CSV-File converted into a pandas Dataframe.
+    '''
+    return pd.read_csv(data)
+
+
+def main():
+    st.title("Small Tableau")
+
+
+
+if __name__ == '__main__':
+    main()
